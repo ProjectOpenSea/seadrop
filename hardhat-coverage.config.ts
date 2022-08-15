2eed48cfdeacc6a5c02358f1fa8cac818658a679
Ryan Ghods <ryan@ryanio.com>
2022-08-15 10:06:39 -0700

    start hardhat tests

--- hardhat-coverage.config.ts
+++ hardhat-coverage.config.ts
@@ -1,13 +1,27 @@
+import fs from "fs";
 import type { HardhatUserConfig } from "hardhat/config";
 
 import "dotenv/config";
 import "@nomiclabs/hardhat-waffle";
 import "@typechain/hardhat";
 import "hardhat-gas-reporter";
+import "hardhat-preprocessor";
 import "solidity-coverage";
 
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
+
+// Configure remappings.
+// https://book.getfoundry.sh/config/hardhat
+// Re-run `forge remappings > remappings.txt`
+// every time you modify libraries in Foundry.
+function getRemappings() {
+  return fs
+    .readFileSync("remappings.txt", "utf8")
+    .split("\n")
+    .filter(Boolean) // remove empty lines
+    .map((line: string) => line.trim().split("="));
+}
 
 const config: HardhatUserConfig = {
   solidity: {
@@ -33,6 +47,21 @@ const config: HardhatUserConfig = {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
   },
+  preprocess: {
+    eachLine: (hre) => ({
+      transform: (line: string) => {
+        if (line.match(/ from "/i)) {
+          getRemappings().forEach(([find, replace]: string[]) => {
+            if (line.match(find)) {
+              line = line.replace(find, replace);
+            }
+          });
+        }
+        return line;
+      },
+    }),
+  },
+  paths: { sources: "./src" },
 };
 
 export default config;
---
+++ test/utils/encoding.ts
@@ -0,0 +1,19 @@
+import { randomBytes as nodeRandomBytes } from "crypto";
+
+const SeededRNG = require("./seeded-rng");
+
+const GAS_REPORT_MODE = process.env.REPORT_GAS;
+
+let randomBytes: (n: number) => string;
+if (GAS_REPORT_MODE) {
+  const srng = SeededRNG.create("gas-report");
+  randomBytes = srng.randomBytes;
+} else {
+  randomBytes = (n: number) => nodeRandomBytes(n).toString("hex");
+}
+
+export const randomHex = (bytes = 32) => `0x${randomBytes(bytes)}`;
+
+export const getRandomNumberBetween = (min: number, max: number) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
