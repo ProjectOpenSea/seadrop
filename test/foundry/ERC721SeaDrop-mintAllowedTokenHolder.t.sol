92dacea9dc4980729ee3062267ae57f38cfc682b
Ryan Ghods <ryan@ryanio.com>
2022-08-22 15:02:50 -0700

    fixes/updates

--- test/foundry/ERC721SeaDrop-mintAllowedTokenHolder.t.sol
+++ test/foundry/ERC721SeaDrop-mintAllowedTokenHolder.t.sol
@@ -34,6 +34,7 @@ contract ERC721DropTest is TestHelper {
                 args.minter != creator &&
                 args.feeRecipient != creator
         );
+        vm.assume(args.numAllowedNftToken > 0 && args.numAllowedNftToken < 5);
         _;
     }
 
