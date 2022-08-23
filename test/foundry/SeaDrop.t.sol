11f04c370440980f7168ac3f79f65797f285c0c3
stephankmin <stephankmin@gmail.com>
2022-08-23 12:35:23 -0700

    fix merge conflict

--- test/foundry/SeaDrop.t.sol
+++ test/foundry/SeaDrop.t.sol
@@ -1,24 +1,6 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.13;
 
-<<<<<<< HEAD
-import "forge-std/Test.sol";
-import { ERC721SeaDrop } from "seadrop/ERC721SeaDrop.sol";
-import { SeaDrop } from "seadrop/SeaDrop.sol";
-import { TestERC721 } from "test/foundry/utils/TestERC721.sol";
-import { SeaDropErrorsAndEvents } from "seadrop/lib/SeaDropErrorsAndEvents.sol";
-
-contract TestSeaDrop is Test, SeaDropErrorsAndEvents {
-    SeaDrop test;
-    ERC721SeaDrop token;
-    TestERC721 badToken;
-
-    function setUp() public {
-        test = new SeaDrop();
-        address[] memory seadrop = new address[](1);
-        seadrop[0] = address(test);
-        token = new ERC721SeaDrop("", "", address(this), seadrop);
-=======
 import { TestHelper } from "test/foundry/utils/TestHelper.sol";
 
 import { ERC721SeaDrop } from "seadrop/ERC721SeaDrop.sol";
@@ -35,25 +17,16 @@ contract TestSeaDrop is TestHelper {
         token = new ERC721SeaDrop("", "", address(this), allowedSeaDrop);
 
         // Deploy a vanilla ERC721 token.
->>>>>>> main
         badToken = new TestERC721();
     }
 
     function testUpdateDropURI() public {
         string memory uri = "https://example.com/";
-<<<<<<< HEAD
-        vm.expectEmit(true, false, false, true, address(test));
-        emit DropURIUpdated(address(token), uri);
-        vm.prank(address(token));
-        test.updateDropURI(uri);
-        assertEq(test.getDropURI(address(token)), uri);
-=======
         vm.expectEmit(true, false, false, true, address(seadrop));
         emit DropURIUpdated(address(token), uri);
         vm.prank(address(token));
         seadrop.updateDropURI(uri);
         assertEq(seadrop.getDropURI(address(token)), uri);
->>>>>>> main
     }
 
     function testUpdateDropURI_onlyERC721SeaDrop() public {
@@ -65,9 +38,6 @@ contract TestSeaDrop is TestHelper {
                 address(badToken)
             )
         );
-<<<<<<< HEAD
-        test.updateDropURI(uri);
-=======
         seadrop.updateDropURI(uri);
     }
 
@@ -84,6 +54,5 @@ contract TestSeaDrop is TestHelper {
             abi.encodeWithSelector(SignerCannotBeZeroAddress.selector)
         );
         seadrop.updateSigners(newSigners);
->>>>>>> main
     }
 }
