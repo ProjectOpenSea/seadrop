3b9499a5453cfdc365e273dc08f871f5805b1adc
stephankmin <stephankmin@gmail.com>
2022-08-26 10:47:45 -0700

    update shim

--- src/shim/Shim.sol
+++ src/shim/Shim.sol
@@ -3,8 +3,8 @@ pragma solidity ^0.8.7;
 
 /**
  * @dev HardHat doesn't support multiple source folders; so import everything
- * extra that reference tests rely on so they get compiled. Allows for faster
+ * extra that hardhat tests rely on so they get compiled. Allows for faster
  * feedback than running an extra yarn build
  */
 import { ERC721 } from "solmate/tokens/ERC721.sol";
-import { TestERC721 } from "seadrop/test/TestERC721.sol";
+import { TestERC721 } from "../test/TestERC721.sol";
