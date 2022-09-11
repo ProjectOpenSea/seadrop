55a8809960a2112e1902401fe96269636f1e9d7d
Ryan Ghods <ryan@ryanio.com>
2022-09-11 14:55:58 -0700

    update to solidity 0.8.17

--- hardhat-coverage.config.ts
+++ hardhat-coverage.config.ts
@@ -13,7 +13,7 @@ const config: HardhatUserConfig = {
   solidity: {
     compilers: [
       {
-        version: "0.8.16",
+        version: "0.8.17",
         settings: {
           viaIR: false,
           optimizer: {
--- src/lib/SeaDropStructs.sol
+++ src/lib/SeaDropStructs.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.16;
+pragma solidity 0.8.17;
 
 /**
  * @notice A struct defining public drop data.
