f13e64996f1fcd84f47da85c636ec846f41662d0
Ryan Ghods <ryan@ryanio.com>
2022-09-18 17:44:46 -0700

    add ERC721PartnerSeaDropRandomOffset + tests

---
+++ src/ERC721PartnerSeaDropRandomOffset.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { ERC721PartnerSeaDrop } from "./ERC721PartnerSeaDrop.sol";
+
+import { Strings } from "openzeppelin-contracts/contracts/utils/Strings.sol";
+
+/**
+ * @title  ERC721PartnerSeaDropRandomOffset
+ * @author James Wenzel (emo.eth)
+ * @author Ryan Ghods (ralxz.eth)
+ * @author Stephan Min (stephanm.eth)
+ * @notice ERC721PartnerSeaDropRandomOffset is a token contract that extends
+ *         ERC721PartnerSeaDrop a apply a randomOffset to the tokenURI,
+ *         to enable fair metadata reveals.
+ */
+contract ERC721PartnerSeaDropRandomOffset is ERC721PartnerSeaDrop {
+    /// @notice The random offset, between 1 and the MAX_SUPPLY at the time of
+    ///         being set.
+    uint256 public randomOffset;
+
+    /// @notice If the collection has been revealed and the randomOffset has
+    ///         been set.
+    bool public revealed;
+
+    /// @notice Revert when setting the randomOffset if already set.
+    error AlreadyRevealed();
+
+    /// @notice Revert when setting the randomOffset if the collection is
+    ///         not yet fully minted.
+    error NotFullyMinted();
+
+    /**
+     * @notice Deploy the token contract with its name, symbol,
+     *         administrator, and allowed SeaDrop addresses.
+     */
+    constructor(
+        string memory name,
+        string memory symbol,
+        address administrator,
+        address[] memory allowedSeaDrop
+    ) ERC721PartnerSeaDrop(name, symbol, administrator, allowedSeaDrop) {}
+
+    /**
+     * @notice Set the random offset, for a fair metadata reveal. Only callable
+     *         by the owner one time when the total number of minted tokens
+     *         equals the max supply. Should be called at the time of reveal.
+     *         of reveal after secondary market trading pre-reveal.
+     */
+    function setRandomOffset() external onlyOwner {
+        if (revealed) {
+            revert AlreadyRevealed();
+        }
+        if (_totalMinted() != _maxSupply) {
+            revert NotFullyMinted();
+        }
+        // block.difficulty returns PREVRANDAO on Ethereum post-merge
+        // NOTE: do not use this on other chains
+        // randomOffset returns between 1 and MAX_SUPPLY
+        randomOffset =
+            (uint256(keccak256(abi.encode(block.difficulty))) %
+                (_maxSupply - 1)) +
+            1;
+        revealed = true;
+    }
+
+    /**
+     * @notice The token URI, offset by randomOffset, to enable fair metadata
+     *         reveals.
+     *
+     * @param tokenId The token id
+     */
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override
+        returns (string memory)
+    {
+        if (!_exists(tokenId)) {
+            revert URIQueryForNonexistentToken();
+        }
+
+        string memory base = _baseURI();
+        if (bytes(base).length == 0) {
+            return "";
+        } else if (!revealed) {
+            return _baseURI();
+        } else {
+            return
+                string.concat(
+                    base,
+                    "/",
+                    Strings.toString((tokenId + randomOffset) % _maxSupply)
+                );
+        }
+    }
+}
---
+++ test/ERC721PartnerSeaDropRandomOffset.spec.ts
@@ -0,0 +1,136 @@
+import { expect } from "chai";
+import { ethers, network } from "hardhat";
+
+import { randomHex } from "./utils/encoding";
+import { faucet } from "./utils/faucet";
+import { VERSION } from "./utils/helpers";
+import { whileImpersonating } from "./utils/impersonate";
+
+import type {
+  ERC721PartnerSeaDropRandomOffset,
+  ISeaDrop,
+} from "../typechain-types";
+import type { Wallet } from "ethers";
+
+describe(`ERC721PartnerSeaDropRandomOffset (v${VERSION})`, function () {
+  const { provider } = ethers;
+  let seadrop: ISeaDrop;
+  let token: ERC721PartnerSeaDropRandomOffset;
+  let owner: Wallet;
+  let admin: Wallet;
+  let creator: Wallet;
+  let minter: Wallet;
+
+  after(async () => {
+    await network.provider.request({
+      method: "hardhat_reset",
+    });
+  });
+
+  before(async () => {
+    // Set the wallets
+    owner = new ethers.Wallet(randomHex(32), provider);
+    admin = new ethers.Wallet(randomHex(32), provider);
+    creator = new ethers.Wallet(randomHex(32), provider);
+    minter = new ethers.Wallet(randomHex(32), provider);
+
+    // Add eth to wallets
+    for (const wallet of [owner, admin, minter, creator]) {
+      await faucet(wallet.address, provider);
+    }
+
+    // Deploy SeaDrop
+    const SeaDrop = await ethers.getContractFactory("SeaDrop", owner);
+    seadrop = await SeaDrop.deploy();
+  });
+
+  beforeEach(async () => {
+    // Deploy token
+    const ERC721PartnerSeaDropRandomOffset = await ethers.getContractFactory(
+      "ERC721PartnerSeaDropRandomOffset",
+      owner
+    );
+    token = await ERC721PartnerSeaDropRandomOffset.deploy(
+      "",
+      "",
+      admin.address,
+      [seadrop.address]
+    );
+  });
+
+  it("Should only let the owner call setRandomOffset once the max supply is reached", async () => {
+    await token.setMaxSupply(100);
+
+    await expect(token.connect(admin).setRandomOffset()).to.be.revertedWith(
+      "OnlyOwner()"
+    );
+
+    await expect(token.connect(owner).setRandomOffset()).to.be.revertedWith(
+      "NotFullyMinted()"
+    );
+
+    // Mint to the max supply.
+    await whileImpersonating(
+      seadrop.address,
+      provider,
+      async (impersonatedSigner) => {
+        await token
+          .connect(impersonatedSigner)
+          .mintSeaDrop(minter.address, 100);
+      }
+    );
+
+    await expect(token.connect(admin).setRandomOffset()).to.be.revertedWith(
+      "OnlyOwner()"
+    );
+
+    expect(await token.randomOffset()).to.equal(ethers.constants.Zero);
+
+    await token.connect(owner).setRandomOffset();
+
+    await expect(token.connect(owner).setRandomOffset()).to.be.revertedWith(
+      "AlreadyRevealed()"
+    );
+
+    expect(await token.randomOffset()).to.not.equal(ethers.constants.Zero);
+  });
+
+  it("Should return the tokenURI correctly offset by randomOffset", async () => {
+    await token.setMaxSupply(100);
+
+    await expect(token.tokenURI(1)).to.be.revertedWith(
+      "URIQueryForNonexistentToken()"
+    );
+
+    // Mint to the max supply.
+    await whileImpersonating(
+      seadrop.address,
+      provider,
+      async (impersonatedSigner) => {
+        await token
+          .connect(impersonatedSigner)
+          .mintSeaDrop(minter.address, 100);
+      }
+    );
+
+    expect(await token.tokenURI(1)).to.equal("");
+
+    await token.setBaseURI("http://example.com");
+
+    expect(await token.tokenURI(1)).to.equal("http://example.com");
+
+    await token.connect(owner).setRandomOffset();
+
+    const randomOffset = await token.randomOffset();
+
+    expect(randomOffset.toNumber()).to.be.greaterThan(0);
+    expect(randomOffset.toNumber()).to.be.lessThanOrEqual(100);
+
+    expect(await token.tokenURI(1)).to.equal(
+      `http://example.com/${(1 + randomOffset.toNumber()) % 100}`
+    );
+    expect(await token.tokenURI(100)).to.equal(
+      `http://example.com/${(100 + randomOffset.toNumber()) % 100}`
+    );
+  });
+});
