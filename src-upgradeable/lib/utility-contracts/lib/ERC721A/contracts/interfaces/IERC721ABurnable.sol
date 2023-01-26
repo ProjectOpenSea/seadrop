13a4491a01d4925bb50e25f5181102dfbaab5cdd
Ryan Ghods <ryan@ryanio.com>
2023-01-25 22:08:14 -0800

    remove utility-contracts lib

--- src-upgradeable/lib/utility-contracts/lib/ERC721A/contracts/interfaces/IERC721ABurnable.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-// ERC721A Contracts v4.2.2
-// Creator: Chiru Labs
-
-pragma solidity ^0.8.4;
-
-import '../extensions/IERC721ABurnable.sol';
--- src-upgradeable/lib/utility-contracts/lib/openzeppelin-contracts/contracts/utils/cryptography/draft-EIP712.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-
-pragma solidity ^0.8.0;
-
-// EIP-712 is Final as of 2022-08-11. This file is deprecated.
-
-import "./EIP712.sol";
