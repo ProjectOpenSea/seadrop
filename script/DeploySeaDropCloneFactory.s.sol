e998d672b7f82344175245ba6c3c00df79eec5f5
James Wenzel <jameswenzel@users.noreply.github.com>
2023-03-08 19:50:21 -0500

    update deploy script

--- script/DeploySeaDropCloneFactory.s.sol
+++ script/DeploySeaDropCloneFactory.s.sol
@@ -8,18 +8,21 @@ import {
 import { BaseCreate2Script } from "create2-scripts/BaseCreate2Script.s.sol";
 
 contract DeploySeaDropCloneFactory is BaseCreate2Script {
+    event log(bytes32);
+
     function run() public {
         setUp();
         vm.createSelectFork("mainnet");
-        bytes32 salt = 0x00000000000000000000000000000000000000002880aa7f8ae2ea08631058e6;
+        // emit log(keccak256(type(ERC721SeaDropCloneFactory).creationCode));
+        bytes32 salt = 0x0000000000000000000000000000000000000000668b68e73536f804134f637c;
         address deployed = _immutableCreate2IfNotDeployed(
             address(deployer),
             salt,
             type(ERC721SeaDropCloneFactory).creationCode
         );
         require(
-            deployed == 0x00000000C2f8CB9a79739832b90D9718219833dE,
-            "did not deploy to correct"
+            deployed == 0x0000000064335cB434247c1143a3695b6DFe1FEf,
+            "did not deploy to correct address"
         );
     }
 }
