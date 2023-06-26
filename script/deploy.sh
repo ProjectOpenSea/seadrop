67f008852a26a5b033bb3a31b5a0bdc1b45c35ee
Vadim Fadeev <xboxfadeev@gmail.com>
2023-06-26 15:13:01 +0100

    feat: add new deployment

--- script/deploy.sh
+++ script/deploy.sh
@@ -3,3 +3,6 @@ forge script script/DeployRaribleDrop.s.sol:DeployRaribleDrop --rpc-url $GOERLI_
 
 forge script script/DeployRaribleDropCloneFactory.s.sol:DeployRaribleDropCloneFactory --rpc-url $POLYGON_RPC_URL --private-key $PRIVATE_KEY -vvvv --broadcast --etherscan-api-key $ETHERSCAN_POLYGON --verify
 forge script script/DeployRaribleDropCloneFactory.s.sol:DeployRaribleDropCloneFactory --rpc-url $GOERLI_RPC_URL --private-key $PRIVATE_KEY -vvvv --broadcast --etherscan-api-key $ETHERSCAN_GOERLI --verify
+
+forge script script/DeployCollectionWithFactory.s.sol:DeployCollectionWithFactory --rpc-url $POLYGON_RPC_URL --private-key $PRIVATE_KEY -vvvv --broadcast --etherscan-api-key $ETHERSCAN_POLYGON --verify
+forge script script/DeployCollectionWithFactory.s.sol:DeployCollectionWithFactory --rpc-url $GOERLI_RPC_URL --private-key $PRIVATE_KEY -vvvv --broadcast --etherscan-api-key $ETHERSCAN_GOERLI --verify
