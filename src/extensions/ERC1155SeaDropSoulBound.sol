07a069e6079be7b269fb03a6808229ed9715ff55
Michael Cohen <michael.cohen@opensea.io>
2023-08-07 12:20:00 -0400

    Soulbound extensions

---
+++ src/extensions/ERC1155SeaDropSoulBound.sol
@@ -0,0 +1,61 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { ERC721SeaDrop } from "../ERC721SeaDrop.sol";
+
+/**
+ * @title  ERC721SeaDropBurnable
+ * @author James Wenzel (emo.eth)
+ * @author Ryan Ghods (ralxz.eth)
+ * @author Stephan Min (stephanm.eth)
+ * @author Michael Cohen (notmichael.eth)
+ * @notice ERC1155SeaDropSoulBound is a token contract that extends
+ *         ERC1155SeaDrop to prevent minted tokens from being transferred after mint.
+ */
+ 
+error SoulboundTransferNotAllowed() 
+
+contract ERC1155SeaDropSoulBound is ERC1155SeaDrop {
+    /**
+     * @notice Deploy the token contract with its name, symbol,
+     *         and allowed SeaDrop addresses.
+     */
+    constructor(
+        address allowedConfigurer,
+        address allowedConduit,
+        address allowedSeaport,
+        string memory name,
+        string memory symbol
+    ) ERC1155SeaDrop(allowedConfigurer, allowedConduit, allowedSeaport, name, symbol) {}
+
+    function transferFrom(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) public override {
+        revert SoulboundTransferNotAllowed();
+    }
+
+    function safeTransferFrom(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) public override {
+        revert SoulboundTransferNotAllowed();
+    }
+
+    function safeBatchTransferFrom(
+        address,
+        address,
+        uint256,
+        uint256,
+        bytes calldata
+    ) public virtual override{
+        revert SoulboundTransferNotAllowed();
+    }
+
+}
---
+++ src/extensions/ERC721SeaDropSoulBound.sol
@@ -0,0 +1,45 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.17;
+
+import { ERC721SeaDrop } from "../ERC721SeaDrop.sol";
+
+/**
+ * @title  ERC721SeaDropBurnable
+ * @author James Wenzel (emo.eth)
+ * @author Ryan Ghods (ralxz.eth)
+ * @author Stephan Min (stephanm.eth)
+ * @author Michael Cohen (notmichael.eth)
+ * @notice ERC721SeaDropSoulBound is a token contract that extends
+ *         ERC721SeaDrop to prevent minted tokens from being transferred after mint.
+ */
+ 
+error SoulboundTransferNotAllowed() 
+
+contract ERC721SeaDropSoulBound is ERC721SeaDrop {
+    /**
+     * @notice Deploy the token contract with its name, symbol,
+     *         and allowed SeaDrop addresses.
+     */
+    constructor(
+        string memory name,
+        string memory symbol,
+        address[] memory allowedSeaDrop
+    ) ERC721SeaDrop(name, symbol, allowedSeaDrop) {}
+
+    function transferFrom(
+        address from,
+        address,
+        uint256
+    ) public override {
+        revert SoulboundTransferNotAllowed();
+    }
+
+    function safeTransferFrom(
+        address from,
+        address,
+        uint256
+    ) public override {
+        revert SoulboundTransferNotAllowed();
+    }
+
+}
